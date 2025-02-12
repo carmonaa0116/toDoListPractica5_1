c7b4254e7fbea137a6d127bc33aaa5c69764922a
carmonaa0116 <96428614+carmonaa0116@users.noreply.github.com>
2025-02-12 10:47:39 +0100

    Update conectar.php

--- php/conectar.php
+++ php/conectar.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername = "sql209.infinityfree.com";
-$username = "if0_38256311";
-$password = "YMzHkeF1hTqY7e";
-$dbname = "if0_38256311_todolist"; // Nombre correcto de la BD
+$servername = "sql7.freesqldatabase.com";
+$username = "sql7762333";
+$password = "qsTbCn6QDM";
+$dbname = "sql7762333"; // Nombre correcto de la BD
 
 // Crear conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
